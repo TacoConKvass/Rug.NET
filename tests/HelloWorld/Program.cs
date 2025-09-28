eb98ad8839b987574cc7e5040af8527efba34920
TacoConKvass <e_frun@o2.pl>
2025-09-28 10:45:14 +0000

    [Add] Basic HelloWorld test to compare output

---
+++ tests/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public struct Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+    }
+}
