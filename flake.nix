ef55f946443e1b57f88dc631234a8baca657f7c9
TacoConKvass <e_frun@o2.pl>
2025-11-26 00:23:08 +0000

    Change: Upgrade flake; Add: dev shell

--- flake.nix
+++ flake.nix
@@ -1,37 +1,42 @@
 {
-    description = "A Rug compiler for the .NET platform";
-    
-    inputs = {
-        stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-        unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
+  description = "";
 
-    outputs = { self, stable, unstable }: let
-        systems = [ "x86_64-linux" "aarch64-linux" ];
-        lib = stable.lib;
-        pkg = lib.genAttrs systems (system: {
-            stable = stable.legacyPackages.${system};
-            unstable = unstable.legacyPackages.${system};
-        });
+  inputs = {
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, unstable } : let
+    systems = [ "x86_64-linux" "aarch64-linux"];
+    lib = unstable.lib;
+    forEachSystem = func: lib.foldAttrs (item: acc: item // acc) {} (lib.map func systems);
+  in forEachSystem (system: let
+      pkgs = unstable.legacyPackages.${system};
     in {
-        packages = lib.genAttrs systems (system: let
-            pkgs = pkg.${system};
-            zig = pkgs.unstable.zig;
-        in {
-            rug = pkgs.unstable.zigStdenv.mkDerivation {
-                inherit system;
-                name = "rug";
-                src = ./.;
-                buildInputs = [ zig ];
+      packages.${system} = rec {
+        default = rug;
+        
+        rug = pkgs.zigStdenv.mkDerivation {
+          inherit system;
+          name = "rug";
+          src = ./.;
+          buildInputs = [ pkgs.zig ];
 
-                buildPhase = ''
-                    runHook preBuild
+          buildPhase = ''
+            runHook preBuild
 
-                    zig build -Doptimize=ReleaseSafe --global-cache-dir $(pwd)/.global-cache -p $out
-                    
-                    runHook postBuild
-                '';
-            };
-        }); 
-    };
+            zig build -Doptimize=ReleaseSafe --global-cache-dir $(pwd)/.global-cache -p $out
+
+            runHook postBuild
+          '';
+        };
+      };
+      
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = [ pkgs.zig pkgs.zls ];
+        shellHook = ''
+          echo "Entered Rug.NET development shell"
+        '';
+      };
+    }
+  );
 }
