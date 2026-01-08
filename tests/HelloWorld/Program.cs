5f835ddd4869224555dc7ad8cf7d96046c5d3e57
TacoConKvass <e_frun@o2.pl>
2026-01-08 21:45:44 +0000

    Delete: Useless tests!

--- tests/HelloWorld/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-public struct Program {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello World from C#");
-    }
-}
